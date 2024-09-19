7326ba59b76de720ebbcee28c1898dbacecab576
Sebastian Luft <sebastian.luft.gm@gmail.com>
2024-09-19 15:52:19 +0200

    Done

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,21 +1,29 @@
 // Iteration 1 | Find the Maximum
-function maxOfTwoNumbers() {}
-
-
-
+function maxOfTwoNumbers(number1,number2) {
+    return Math.max(number1,number2);
+}
 
 // Iteration 2 | Find the Longest Word
 const words = ["mystery", "brother", "aviator", "crocodile", "pearl", "orchard", "crackpot"];
 
-function findLongestWord() {}
-
+function findLongestWord(wordArray) {
+    if (wordArray.length === 0) return null;
+    let wordArrayLength = []
+    wordArrayLength = wordArray.map((el) => el.length);
+    longestWord = Math.max(...wordArrayLength); // unpacking array w/ ...
+    return wordArray[wordArrayLength.indexOf(longestWord)];
+}
 
 
 
 // Iteration 3 | Sum Numbers
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
-function sumNumbers() {}
+function sumNumbers(numberArray) {
+    let sum = 0;
+    numberArray.forEach(element => {sum += element});
+    return sum;
+    };
 
 
 
@@ -23,7 +31,10 @@ function sumNumbers() {}
 // Iteration 4 | Numbers Average
 const numbers2 = [2, 6, 9, 10, 7, 4, 1, 9];
 
-function averageNumbers() {}
+function averageNumbers(numberArray) {
+    if (numberArray.length === 0) return 0;
+    return sumNumbers(numberArray)/numberArray.length
+}
 
 
 
@@ -31,4 +42,8 @@ function averageNumbers() {}
 // Iteration 5 | Find Elements
 const words2 = ["machine", "subset", "trouble", "starting", "matter", "eating", "truth", "disobedience"];
 
-function doesWordExist() {}
+function doesWordExist(wordArray, searchTerm) {
+    if (wordArray.indexOf(searchTerm) != -1) return true;
+    if (wordArray.length === 0) return null;
+    return false;
+}
